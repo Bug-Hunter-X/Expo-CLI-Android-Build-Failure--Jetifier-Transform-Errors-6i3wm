cb814897842a203eeb45d55f6c735beecf6e5221
BugHunter-X <askzenai@gmail.com>
2025-02-22 06:30:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+This error occurs when using the Expo CLI to build an Android app and involves the `jetifier` transform.  The error message might vary, but typically indicates issues with converting Android libraries to be compatible with AndroidX.
+
+Example Error (the specific error message can vary):
+```
+> Task :app:transformClassesWithJetifierForDebug FAILED
+... some error messages from jetifier ...
+```
